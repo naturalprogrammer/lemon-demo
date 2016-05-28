75224bb45589f33fbec4f69ca2e0cabe9ca8ee27
Sanjay <skpatel20@gmail.com>
2016-05-28 17:21:52 +0530

    Trying spring-lemon-social

--- src/main/java/com/naturalprogrammer/spring/lemondemo/MySecurityConfig.java
+++ src/main/java/com/naturalprogrammer/spring/lemondemo/MySecurityConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import com.naturalprogrammer.spring.lemon.security.LemonSecurityConfig;
+import com.naturalprogrammer.spring.lemon.social.LemonSocialSecurityConfig;
 
 @Configuration
-public class MySecurityConfig extends LemonSecurityConfig {
+public class MySecurityConfig extends LemonSocialSecurityConfig {
 
 	@Override
 	protected void authorizeRequests(HttpSecurity http) throws Exception {
