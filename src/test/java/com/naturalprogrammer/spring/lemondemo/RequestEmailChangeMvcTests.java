1f7ccdb73046cd740855675979c4faed1cc61f50
Sanjay Patel <skpatel20@gmail.com>
2018-02-28 05:48:49 +0530

    Completed Request-Email-Change tests

--- src/test/java/com/naturalprogrammer/spring/lemondemo/RequestEmailChangeMvcTests.java
+++ src/test/java/com/naturalprogrammer/spring/lemondemo/RequestEmailChangeMvcTests.java
@@ -1,12 +1,17 @@
 package com.naturalprogrammer.spring.lemondemo;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.naturalprogrammer.spring.lemon.security.LemonSecurityConfig;
 import com.naturalprogrammer.spring.lemon.util.LemonUtils;
 import com.naturalprogrammer.spring.lemondemo.entities.User;
@@ -23,7 +28,7 @@ public class RequestEmailChangeMvcTests extends AbstractMvcTests {
 		
 		return user;
 	}
-	
+
 	@Test
 	public void testRequestEmailChange() throws Exception {
 		
@@ -36,5 +41,146 @@ public class RequestEmailChangeMvcTests extends AbstractMvcTests {
 		User updatedUser = userRepository.findById(UNVERIFIED_USER_ID).get();
 		Assert.assertEquals(NEW_EMAIL, updatedUser.getNewEmail());
 		Assert.assertEquals(UNVERIFIED_USER_EMAIL, updatedUser.getEmail());
+	}
+	
+	/**
+     * A good admin should be able to request changing email of another user.
+     */
+	@Test
+	public void testGoodAdminRequestEmailChange() throws Exception {
+		
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(ADMIN_ID))
+				.content(LemonUtils.toJson(form())))
+				.andExpect(status().is(204));
+		
+		User updatedUser = userRepository.findById(UNVERIFIED_USER_ID).get();
+		Assert.assertEquals(NEW_EMAIL, updatedUser.getNewEmail());
+	}	
+	
+	/**
+     * A request changing email of unknown user.
+     */
+	@Test
+	public void testRequestEmailChangeUnknownUser() throws Exception {
+		
+		mvc.perform(post("/api/core/users/99/email-change-request")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(ADMIN_ID))
+				.content(LemonUtils.toJson(form())))
+				.andExpect(status().is(404));
+	}
+
+	/**
+	 * A non-admin should not be able to request changing
+	 * the email id of another user
+	 */
+	@Test
+	public void testNonAdminRequestEmailChangeAnotherUser() throws Exception {
+		
+		mvc.perform(post("/api/core/users/{id}/email-change-request", ADMIN_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(USER_ID))
+				.content(LemonUtils.toJson(form())))
+				.andExpect(status().is(403));
+		
+		User updatedUser = userRepository.findById(UNVERIFIED_USER_ID).get();
+		Assert.assertNull(updatedUser.getNewEmail());
+	}
+	
+	/**
+	 * A bad admin trying to change the email id
+	 * of another user
+	 */
+	/**
+	 * A non-admin should not be able to request changing
+	 * the email id of another user
+	 */
+	@Test
+	public void testBadAdminRequestEmailChangeAnotherUser() throws Exception {
+		
+		mvc.perform(post("/api/core/users/{id}/email-change-request", ADMIN_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_ADMIN_ID))
+				.content(LemonUtils.toJson(form())))
+				.andExpect(status().is(403));
+	}
+
+	/**
+     * Trying with invalid data.
+	 * @throws Exception 
+	 * @throws JsonProcessingException 
+     */
+	@Test
+	public void tryingWithInvalidData() throws JsonProcessingException, Exception {
+		
+    	// try with null newEmail and password
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_USER_ID))
+				.content(LemonUtils.toJson(new User())))
+				.andExpect(status().is(422))
+				.andExpect(jsonPath("$.errors[*].field").value(allOf(hasSize(2),
+					hasItems("updatedUser.newEmail",
+							 "updatedUser.password"))));
+    	
+		User updatedUser = new User();
+		updatedUser.setPassword("");
+		updatedUser.setNewEmail("");
+		
+    	// try with null newEmail and password
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_USER_ID))
+				.content(LemonUtils.toJson(updatedUser)))
+				.andExpect(status().is(422))
+				.andExpect(jsonPath("$.errors[*].field").value(allOf(hasSize(4),
+					hasItems("updatedUser.newEmail",
+							 "updatedUser.password"))));
+
+		// try with invalid newEmail
+		updatedUser = form();
+		updatedUser.setNewEmail("an-invalid-email");
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_USER_ID))
+				.content(LemonUtils.toJson(updatedUser)))
+				.andExpect(status().is(422))
+				.andExpect(jsonPath("$.errors[*].field").value(allOf(hasSize(1),
+					hasItems("updatedUser.newEmail"))));
+
+		// try with wrong password
+		updatedUser = form();
+		updatedUser.setPassword("wrong-password");
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_USER_ID))
+				.content(LemonUtils.toJson(updatedUser)))
+				.andExpect(status().is(422))
+				.andExpect(jsonPath("$.errors[*].field").value(allOf(hasSize(1),
+					hasItems("updatedUser.password"))));
+
+		// try with null password
+		updatedUser = form();
+		updatedUser.setPassword(null);
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_USER_ID))
+				.content(LemonUtils.toJson(updatedUser)))
+				.andExpect(status().is(422))
+				.andExpect(jsonPath("$.errors[*].field").value(allOf(hasSize(1),
+					hasItems("updatedUser.password"))));
+
+		// try with an existing email
+		updatedUser = form();
+		updatedUser.setNewEmail(ADMIN_EMAIL);;
+		mvc.perform(post("/api/core/users/{id}/email-change-request", UNVERIFIED_USER_ID)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(LemonSecurityConfig.TOKEN_REQUEST_HEADER, tokens.get(UNVERIFIED_USER_ID))
+				.content(LemonUtils.toJson(updatedUser)))
+				.andExpect(status().is(422))
+				.andExpect(jsonPath("$.errors[*].field").value(allOf(hasSize(1),
+					hasItems("updatedUser.newEmail"))));
 	}
 }
